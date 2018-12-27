017fb0dc499a366706970647827b7fbb64c64da8
USER <user@user-no-macbook-pro.local>
2018-12-27 21:30:59 +0900

    Initial Commit

---
+++ Muscle-Gacha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Muscle-Gacha
+//
+//  Created by  USER on 2018/12/27.
+//  Copyright © 2018 ShibusawaTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
